adb4a16098fc231bb231874bf5445cd63da0a8bb
Abdulahi Jama <kararjaamac1@gmail.com>
2024-04-23 15:54:58 -0700

    class and object

---
+++ src/variabales/primitive.java
@@ -0,0 +1,20 @@
+package variabales;
+
+public class primitive {
+    public static void main(String[] args) {
+        // primitive data type
+        // Operations
+
+        double d = 456.5;
+        float f= 23.9f;
+        System.out.println(f);
+        long size = 24254l;
+        System.out.println(size);
+        // + - / * %
+
+        // 10 / 9 = 1
+        int r= 10% 9;
+        System.out.println(r);
+
+    }
+}
