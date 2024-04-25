bd84fdef0af1c1ca387ca88a85ef53826dc103cb
Abdulahi Jama <kararjaamac1@gmail.com>
2024-04-25 15:38:52 -0700

    method and constructor

--- src/methods/Student2.java
+++ src/methods/Student2.java
@@ -1,5 +1,25 @@
 package methods;
 
 public class Student2 {
+    int sid;
+    String sname;
+    char grade;
+    void  printStudentData(){
+        System.out.println("sid + \"sname \" + sname + \"grade + \" +grade = " + sid + "sname " + sname + "grade + " +grade);
+    }
+    /*void setStudentData( int id, String name, char gr){
+        sid= id;
+        sname= name;
+        gr= grade;
+
+
+    } */
+    Student2(int id, String name, char gr){
+
+        sid= id;
+        sname=name;
+        gr= grade;
+
+    }
 
 }
---
+++ src/methods/Student2Main.java
@@ -0,0 +1,24 @@
+package methods;
+
+public class Student2Main {
+    public static void main(String[] args) {
+
+/*Student2 stu= new Student2();
+// 1) using object reference variable
+     stu.sid= 101;
+     stu.sname= "john";
+     stu.grade= 'A';
+     stu.printStudentData();
+     stu.setStudentData(101, " david ",'A');
+     stu.printStudentData(); */
+
+
+
+        Student2 stu= new Student2(11, "muuse", 'A');
+
+        stu.printStudentData();
+
+
+    }
+
+}
