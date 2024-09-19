1ed6327de06e4f6f46f5d856ead82e5574bafc59
Abdulahi Jama <kararjaamac1@gmail.com>
2024-09-18 18:15:04 -0700

    logical operations

---
+++ src/RationalOperations/Operations.java
@@ -0,0 +1,16 @@
+package RationalOperations;
+
+public class Operations {
+    public static void main(String[] args) {
+        boolean b1= 20>10;
+        boolean b2= 20<20;
+        System.out.println(b1 && b2); // false
+        System.out.println(b1 || b2 ); // true
+
+        System.out.println((10>20) && (20>10));// false
+        System.out.println((10>20)|| (20>10)); // true
+
+
+
+    }
+}
