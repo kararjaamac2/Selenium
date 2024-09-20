485afa4fbf045e6c8c5b5e92f78ef3c6f28a4ae0
Abdulahi Jama <kararjaamac1@gmail.com>
2024-09-19 17:36:28 -0700

    post and pre increaments

---
+++ src/RationalOperations/postIncrement.java
@@ -0,0 +1,25 @@
+package RationalOperations;
+
+public class postIncrement {
+    public static void main(String[] args) {
+
+        // pre increament
+
+        int a=10;
+        a=++a;
+        System.out.println(a);
+
+        // post increament
+
+        int b = 10;
+        int b1 = b++;
+
+        System.out.println(b);
+
+
+
+
+
+
+    }
+}
